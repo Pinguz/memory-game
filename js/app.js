c2aafb90d8b48d2ded5b27a51a1c9c379485359d
Pinguz <micky.zjj@gmail.com>
2018-11-10 10:14:21 +0800

    Add Card

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 /*
  * 创建一个包含所有卡片的数组
  */
+let cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
+let openCard = [];
+let moves = 0;
+let starts = 3;
+let matchFound = 0;
+let startGame = false;
 
 /*
  * 显示页面上的卡片
@@ -25,6 +31,64 @@ function shuffle(array) {
     return array;
 }
 
+//循环遍历每张卡片，创建其 HTML
+function createCard(){
+  let cardList = shuffle(cards);
+  cardList.forEach(function(card){
+    $(".deck").append('<li><i class="card fa '+ card + '"></i></li>');
+  })
+}
+
+function findMatch(){
+  //显示点击的卡片
+  $(".card").on("click",function(){
+    if ($(this).hasClass("open show")){ return;}
+    $(this).toggleClass("flipInY open show");
+    openCard.push($(this));
+    startGame = true;
+  //判断卡列表
+    if (openCard.length === 2) {
+      if (openCard[0][0].classList[2]) === openCard[1][0].classList[2]{
+        openCard[0][0].classList.add("bounceIn","match");
+        openCard[1][0].classList.add("bounceIn","match");
+        $(openCard[0]).off('click');
+        $(openCard[1]).off('click');
+        matchFound += 1;
+        moves++;
+        removeOpenCards();
+        findWinner();
+      } else {
+        //如果类不匹配，显示错误提示
+        openCard[0][0].classList.add("shake","wrong");
+        openCard[1][0].classList.add("shake","wrong");
+        moves++;
+      }
+    }
+  updateMoves();
+  })
+}
+
+//刷新步数
+function updateMoves() {
+  if (moves ===1) {
+    $("#movesText").text(" Moves");
+  } else {
+    $("#movesText").text(" Moves");
+  }
+  $("#moves").text(moves.toString());
+
+  if (moves > 0 && move < 16) {
+    startRating = startRating;
+  } else if (moves > 16 && move < 21) {
+    $("#starOne").removeClass("fa-star");
+    startRating = "2";
+  } else if (moves > 21) {
+    $("#starTwo").removeClass("fa-star");
+    startRating = "1";
+  }
+}
+
+//当游戏完成时显示弹层
 
 /*
  * 设置一张卡片的事件监听器。 如果该卡片被点击：
